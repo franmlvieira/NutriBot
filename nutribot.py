23da236048be1019b95df044e375fbed829e219b
franmlvieira <franmlvieira@gmail.com>
2025-02-12 17:11:48 -0300

    Update nutribot.py

--- nutribot.py
+++ nutribot.py
@@ -1,6 +1,4 @@
 from telegram import Update, Bot
-import locale
-locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, JobQueue
 import datetime
 import random
